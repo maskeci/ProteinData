4088bfc743e5435fb63d1a270456c0780a97a703
maskeci <119742567+maskeci@users.noreply.github.com>
2023-01-13 15:37:05 +0300

    Add files via upload

---
+++ EMBL_scrapper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re   
+
+#https://www.uniprot.org/help/release-statistics
+url = "https://www.ebi.ac.uk/uniprot/TrEMBLstats"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+
+div = soup.find("div", {"class": "dynamic-content"})
+import_url = div.get("data-import")
+#This site imports data from main source. This code access to import url
+
+page2 = requests.get(import_url)
+soup2 = BeautifulSoup(page2.content, "html.parser")
+
+body = str(soup2.find("pre"))
+
+start = body.find("Protein existence (PE)")
+end = body.find("\nThe growth ")
+between = body[start:end]
+between = between.split("\n")
+between = list(filter(lambda x:x, between))
+
+between = [re.split(r'\s{2,}', x) for x in between]
+
+between = pd.DataFrame( between[1:6], columns = between[0])
+
+def GetData():
+    return between
+
---
+++ SwissProt_scrapper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re   
+
+#https://www.uniprot.org/help/release-statistics
+url = "https://web.expasy.org/docs/relnotes/relstat.html"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+body = str(soup.find("pre"))
+
+start = body.find("Protein existence (PE)")
+end = body.find("\nThe growth ")
+between = body[start:end]
+
+between = between.split("\n")
+between = list(filter(lambda x:x, between))
+
+between = [re.split(r'\s{2,}', x) for x in between]
+
+between = pd.DataFrame( between[1:6], columns = between[0])
+
+def GetData():
+    return between
+
